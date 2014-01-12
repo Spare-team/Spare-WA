d0a5dc6c11679a5503161b8e307c343c41aff16c
Ricardo <dev.rsierra@gmail.com>
2014-01-12 14:46:10 -0600

    angular controller home

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,25 +1,27 @@
-var Products = angular.module("products", ['ngRoute']);
+var Spare = angular.module("Spare", ['ngRoute']);
 
-Products.controller('product', ['$scope', '$http', 
-	function($scope, $http){
-		$http.get('/products/', {dataType: 'json', headers: {'Content-Type' : 'application/json'}, data:''})
-			.success(function(data,status, header, config){
-				$scope._products = data;	
-			})
-			.error(function(data, status, headers, config){
-				alert("mamaste");
-			});
-	}
+Spare.config(['$routeProvider',
+    function($routeProvider){
+        $routeProvider
+            .when('/',{      
+                controller : 'HomeController',          
+                templateUrl : '/templates/welcome.html',
+            })
+            .when('/products',{
+                controller : 'ProductsController',
+                templateUrl : '/templates/products_all.html',
+            })
+            .otherwise({
+                redirectTo : '/'
+            });
+}]);
+Spare.controller('HomeController',['$scope', function($scope){ }]);
+
+Spare.controller('ProductsController', ['$scope', '$http',
+    function($scope, $http){
+        $http.get('/products', {dataType: 'json', headers: {'Content-Type' : 'application/json'}, data:''})
+            .success(function(data){
+                $scope._products = data;
+            });
+    }
 ]);
-
-Products.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/', {
-      /*  templateUrl: 'partials/phone-detail.html',*/
-        controller: 'product'
-      }).
-      otherwise({
-        redirectTo: '/'
-      });
-  }]);
